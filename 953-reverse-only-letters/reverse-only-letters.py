29a900045d554054148702e3985f53152566ff24
MANOJ PRASAD <velupukondamanojprasad@gmail.com>
2026-09-06 11:30:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (37.71%) - LeetSync

---
+++ 953-reverse-only-letters/reverse-only-letters.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        l=list(s)
+        i=0
+        j=len(l)-1
+        while(i<=j):
+            if not l[i].isalpha():
+                i+=1
+                continue
+            if not l[j].isalpha():
+                j-=1
+                continue
+            l[i],l[j]=l[j],l[i] 
+            i+=1
+            j-=1
+        return "".join(l)           
